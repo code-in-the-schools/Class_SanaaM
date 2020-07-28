de42dbdff388dcb9ba268a1acf6cef9e7cb1a130
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-28 17:31:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+class FavorieCharacter:
+  name = ""
+  personalitytraits = ""
+  height = ""
+  
+tv = []  
+for r in range(3):
+  f = FavorieCharacter
+  f.name = int(input("name?"))
+  f.personalitytraits = int(input("personalitytraits?"))
+  f.height = int(input("height?"))
+
+for r in range(3):
+  print(tv[r].name)
+
